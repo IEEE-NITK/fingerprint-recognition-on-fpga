06a3f6eb4405beac9855fb4e8041c0f137d2284c
KrutiDeepanPanda <krutideepan123@gmail.com>
2020-12-02 21:42:31 +0530

    Separated Design and Testbench

--- 32-bit-register file/32bit_RegFile_tb.v
+++ 32-bit-register file/32bit_RegFile_tb.v
@@ -1,29 +1,3 @@
-module reg32file
-(input clk,
- input reg_write,
- input [4:0] r_reg1,
- input [4:0] r_reg2,
- input [4:0] w_reg,
- input [31:0] w_data,
- output [31:0] r_data1,
- output [31:0] r_data2);
-
- reg [31:0] reg_file [0:31];
-
- 
- assign r_data1 = reg_file[r_reg1];
- assign r_data2 = reg_file[r_reg2];
-
-
- always @(posedge clk)
- begin
-	if(reg_write)
-	begin
-		reg_file[w_reg] <= w_data;
-	end
- end
-endmodule
-
 module reg32file_tb;
 
  reg clk, reg_write;
