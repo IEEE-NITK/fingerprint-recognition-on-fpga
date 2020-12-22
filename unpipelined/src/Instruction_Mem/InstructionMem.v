4eeab3eba68f8c11b0bb42f36f7790c345637a06
KrutiDeepanPanda <krutideepan123@gmail.com>
2020-12-22 19:16:17 +0530

    Reverted to original Instruction Memory

--- unpipelined/src/Instruction_Mem/InstructionMem.v
+++ unpipelined/src/Instruction_Mem/InstructionMem.v
@@ -10,7 +10,7 @@
 
 module InstructionMemory(Address, Instruction); 
 
-    input       [7:0]  Address;        // Input Address 
+    input       [31:0]  Address;        // Input Address 
 
     output   [31:0]  Instruction;    // Instruction at memory location Address
     
