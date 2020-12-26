bb5359aa76d64146508b410d4e9325a267119972
Rahul Magesh <60037964+RAZ4AMZ@users.noreply.github.com>
2020-12-26 15:43:45 +0530

    Update 32bit_RegFile_Design.v
    Changed Variable names

--- unpipelined/src/32-bit-register file/32bit_RegFile_Design.v
+++ unpipelined/src/32-bit-register file/32bit_RegFile_Design.v
@@ -1,25 +1,25 @@
 module reg32file
 (input clk,
- input reg_write,
- input [4:0] r_reg1,
- input [4:0] r_reg2,
- input [4:0] w_reg,
- input [31:0] w_data,
- output [31:0] r_data1,
- output [31:0] r_data2);
+ input WERF,
+ input [4:0] Ra,
+ input [4:0] RA2,
+ input [4:0] WA,
+ input [31:0] WD,
+ output [31:0] RD1,
+ output [31:0] RD2);
 
  reg [31:0] reg_file [0:31];
 
  
- assign r_data1 = reg_file[r_reg1];
- assign r_data2 = reg_file[r_reg2];
+ assign RD1 = reg_file[Ra];
+ assign RD2 = reg_file[RA2];
 
 
  always @(posedge clk)
  begin
-	if(reg_write)
+	if(WERF)
 	begin
-		reg_file[w_reg] <= w_data;
+		reg_file[WA] <= WD;
 	end
  end
 endmodule
