1331c419a65f35cace08114653de4a41c0e0d2c4
Rahul Magesh <60037964+RAZ4AMZ@users.noreply.github.com>
2020-12-26 12:46:02 +0530

    Update DataMem.v
    1. Removed readmemb
    2. Increased Memory size

--- unpipelined/src/Data_Mem/DataMem.v
+++ unpipelined/src/Data_Mem/DataMem.v
@@ -6,26 +6,21 @@ module Data_Memory(
  
  // write port
  input [31:0]   WD,
- input We,
- input Oe,
+ input WE,
+ input OE,
  // read port
  output [31:0]   RD
 );
 
-reg [31:0] memory [31:0]; 
+reg [31:0] memory [2^30-1:0]; 
 //check no. of rows
 
 wire [5:0] ram_addr=Adr[5:0];
 
-initial
- begin
-  $readmemb("./test/test.data", memory);
- end
- 
  always @(posedge clk) begin
-  if (We)
-   memory[ram_addr] <= Wd;
+  if (WE)
+   memory[ram_addr] <= WD;
  end
- assign RD = (Oe==1'b1) ? memory[ram_addr]: 32'd0; 
+ assign RD = (OE==1'b1) ? memory[ram_addr]: 32'd0; 
 
 endmodule
