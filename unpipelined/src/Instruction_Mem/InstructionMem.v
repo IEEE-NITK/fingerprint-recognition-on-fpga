26f90d25368a9afa4a928dc290bb473a9b97737a
krutideepanpanda <krutideepan123@gmail.com>
2020-12-27 16:03:50 +0530

    Fixed instruction memory

--- unpipelined/src/Instruction_Mem/InstructionMem.v
+++ unpipelined/src/Instruction_Mem/InstructionMem.v
@@ -8,13 +8,12 @@
 // Instruction: 32-Bit output port.
 ////////////////////////////////////////////////////////////////////////////////
 
-module InstructionMemory( IA, ID, [7:0] mem[0:1024] ); 
-
-    input [31:0]  IA;        // Input Address 
-
-    output   [31:0]  ID;    // Instruction at memory location Address
+module InstructionMemory( 
+	input [31:0]  IA,        // Input Address 
+	output  [31:0]  ID    // Instruction at memory location Address
+	); 
     
-    input [7:0] mem[0:1024]; //check what to do
+    reg [7:0] mem[0:1024]; //check what to do
 
     assign ID = mem[IA>>2];	
 	
