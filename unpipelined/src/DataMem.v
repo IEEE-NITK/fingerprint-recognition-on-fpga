ac83eed24e38a31b25ab1281bc85c0929c01129a
krutideepanpanda <krutideepan123@gmail.com>
2021-03-23 20:13:21 +0530

    aes add attempt 1

--- unpipelined/src/DataMem.v
+++ unpipelined/src/DataMem.v
@@ -4,13 +4,13 @@
 module Data_Memory(
  input clk,
  input [31:0]   Adr,
- input [31:0]   MWD,
+ input [127:0]   MWD,
  input MWR,
  input MOE,
- output [31:0]   MRD
+ output [127:0]   MRD
 );
 
-reg [31:0] memory [1023:0]; 
+reg [127:0] memory [1023:0]; 
 
 wire [5:0] ram_addr=Adr[5:0];
 
@@ -18,5 +18,5 @@ wire [5:0] ram_addr=Adr[5:0];
   if (MWR)
    memory[ram_addr] <= MWD;
  end
- assign MRD = (MOE==1'b1) ? memory[ram_addr]: 32'd0; 
+ assign MRD = (MOE==1'b1) ? memory[ram_addr]: 128'd0; 
 endmodule
