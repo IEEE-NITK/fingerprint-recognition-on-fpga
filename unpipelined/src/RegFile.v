027b64b7605d0480754e96965e15a2a76fcc9053
krutideepanpanda <krutideepan123@gmail.com>
2021-03-26 11:39:41 +0530

    ignore this commit

--- unpipelined/src/RegFile.v
+++ unpipelined/src/RegFile.v
@@ -1,30 +1,26 @@
 `timescale 1ns / 1ps
 
-// 32-bit register file containing 32 registers
-// Writeback at negative edge of clock
+// A register bank with 32, 32-bit registers 
 
-module reg32file
-(input clk,
- input WERF,
- input [4:0] RA1,
- input [4:0] RA2,
- input [4:0] WA,
- input [31:0] WD,
- output [31:0] RD1,
- output [31:0] RD2);
-
- reg [31:0] reg_file [31:0];
-
- 
- assign RD1 = reg_file[RA1];
- assign RD2 = reg_file[RA2];
-
-
- always @(negedge clk)
- begin
-	if(WERF)
-	begin
-		reg_file[WA] <= WD;
-	end
- end
+module regfile(
+    input clk,
+    input [4:0] RA1,
+    input [4:0] RA2,
+    input [4:0] WA,
+    input [31:0] WD,
+    input WERF,
+    output [31:0] RD1,
+    output [31:0] RD2
+    );
+    
+    reg [31:0] reg_file [31:0];  // 32 32-bit registers
+    
+    assign RD1 = reg_file[RA1];
+    assign RD2 = reg_file[RA2];
+    
+    always @(posedge clk)
+    begin
+        if(WERF)
+            reg_file[WA]<=WD;
+    end
 endmodule
--- unpipelined/src/regfile.v
+++ unpipelined/src/regfile.v
@@ -4,23 +4,23 @@
 
 module regfile(
     input clk,
-    input [4:0] readreg1,
-    input [4:0] readreg2,
-    input [4:0] writereg,
-    input [31:0] wdata,
-    input w_en,
-    output [31:0] rdata1,
-    output [31:0] rdata2
+    input [4:0] RA1,
+    input [4:0] RA2,
+    input [4:0] WA,
+    input [31:0] WD,
+    input WERF,
+    output [31:0] RD1,
+    output [31:0] RD2
     );
     
-    reg [31:0] regbank [31:0];  // 32 32-bit registers
+    reg [31:0] reg_file [31:0];  // 32 32-bit registers
     
-    assign rdata1 = regbank[readreg1];
-    assign rdata2 = regbank[readreg2];
+    assign RD1 = reg_file[RA1];
+    assign RD2 = reg_file[RA2];
     
     always @(posedge clk)
     begin
-        if(w_en)
-            regbank[writereg]<=wdata;
+        if(WERF)
+            reg_file[WA]<=WD;
     end
 endmodule
