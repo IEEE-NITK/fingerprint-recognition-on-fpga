79d6da5a968fa3f7cee4b09411e67889905003e1
krutideepanpanda <krutideepan123@gmail.com>
2021-05-25 15:12:02 +0530

    fixed for video submission

--- unpipelined/examples/sumFirstTenNumbers/beta_tb.v
+++ unpipelined/examples/sumFirstTenNumbers/beta_tb.v
@@ -17,12 +17,12 @@ module beta_tb;
         for (i=0;i<32;i=i+1) begin
             uut.regfile.reg_file[i] = i;
         end
-        //uut.dm.memory[0] = 128'haa29df7d196f03aa5e36698ba569b133;   //0
-        //uut.dm.memory[1] = 128'h32ed0c201c35720637564e513590b443;   //10
-        //uut.dm.memory[2] = 128'h280549e1f9a45a9c886cb6eb09223ca8;   //20
-        //uut.dm.memory[3] = 128'hf03fe8d863e6b1d24cf441166ea4ac92;   //30
-        //uut.dm.memory[4] = 128'h3f44d681c1651ea47d639a959e27654e;   //40
-        $readmemb("data.mem", uut.dm.memory);
+        uut.dm.memory[0] = 128'haa29df7d196f03aa5e36698ba569b133;   //0
+        uut.dm.memory[1] = 128'h32ed0c201c35720637564e513590b443;   //10
+        uut.dm.memory[2] = 128'h280549e1f9a45a9c886cb6eb09223ca8;   //20
+        uut.dm.memory[3] = 128'hf03fe8d863e6b1d24cf441166ea4ac92;   //30
+        uut.dm.memory[4] = 128'h3f44d681c1651ea47d639a959e27654e;   //40
+        //$readmemb("data.mem", uut.dm.memory);
         $readmemb("final_enc.mem", uut.im.mem);
         
         $display($time, " Register%1d: %d", 0, uut.regfile.reg_file[0]);
